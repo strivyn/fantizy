78931f8664316ea7e747f48589cbd6aedbcafb63
strivyn <edward@strivyn.com>
2026-08-05 07:52:00 -0700

    Add files via upload

---
+++ app/waivers/page.js
@@ -0,0 +1,15 @@
+import { load } from "../../lib/page";
+import Board from "../../components/Board";
+import { Figures } from "../../components/Shell";
+export const revalidate = 1800;
+export const metadata = {
+  title: "Waivers",
+  alternates: { canonical: "/waivers" },
+};
+export default async function P() {
+  const d = await load();
+  return (<>
+    <div className="hero inner"><Figures d={d} /></div>
+    <Board board={d.board} adds={d.adds} drops={d.drops} injuries={d.injuries} view="waivers" />
+  </>);
+}
